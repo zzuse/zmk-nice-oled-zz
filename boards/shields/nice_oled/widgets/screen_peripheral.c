f741e532e79ea06bb51e101a8c909dcecedce606
zzuse <zzuse@outlook.com>
2026-03-04 21:30:43 -0600

    make keyboard work for text vertical labels

--- boards/shields/nice_oled/widgets/screen_peripheral.c
+++ boards/shields/nice_oled/widgets/screen_peripheral.c
@@ -1,7 +1,4 @@
 #include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
-LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
-
 #include <zmk/battery.h>
 #include <zmk/display.h>
 #include <zmk/event_manager.h>
@@ -13,88 +10,99 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include "screen_peripheral.h"
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
+static lv_obj_t *global_logic_canvas = NULL;
+static lv_obj_t *global_phys_canvas = NULL;
+static int global_draw_count = 0;
 
-static lv_obj_t *logic_canvas_peri;
-LV_DRAW_BUF_DEFINE_STATIC(logic_draw_buf_peri, 128, 128, LV_COLOR_FORMAT_I1);
-
-static void draw_text(int x, int y, const char *text) {
+static void draw_text(lv_obj_t *target_canvas, int x, int y, const char *text)
+{
     lv_draw_label_dsc_t label_dsc;
     lv_draw_label_dsc_init(&label_dsc);
-    label_dsc.color = LVGL_FOREGROUND;
+    label_dsc.color = lv_color_make(0, 0, 0); // Black text (Index 0)
     label_dsc.font = &lv_font_unscii_8;
-    
+
     lv_layer_t layer;
-    lv_canvas_init_layer(logic_canvas_peri, &layer);
-    // Logical drawing area is 32px wide, canvas is 128px high
-    lv_area_t coords = {x, y, 31, (y + 20 > 127) ? 127 : y + 20};
+    lv_canvas_init_layer(target_canvas, &layer);
+
+    // Width is exactly 32px. Limit height to prevent wrapping.
+    lv_area_t area = {x, y, 31, y + 10};
     label_dsc.text = text;
-    lv_draw_label(&layer, &label_dsc, &coords);
-    lv_canvas_finish_layer(logic_canvas_peri, &layer);
+    lv_draw_label(&layer, &label_dsc, &area);
+
+    lv_canvas_finish_layer(target_canvas, &layer);
 }
 
-static void draw_full_canvas(lv_obj_t *widget_obj) {
-    if (!logic_canvas_peri) return;
-    lv_obj_t *phys_canvas = lv_obj_get_child(widget_obj, 0);
-    lv_canvas_fill_bg(logic_canvas_peri, LVGL_BACKGROUND, LV_OPA_COVER);
-    
-    char bat_buf[16];
-    snprintf(bat_buf, sizeof(bat_buf), "PERI: %d%%", zmk_battery_state_of_charge());
-    draw_text(2, 5, bat_buf);
-    
-    draw_text(2, 55, zmk_split_bt_peripheral_is_connected() ? "CONN" : "DISC");
+static void draw_canvas(void)
+{
+    if (!global_logic_canvas || !global_phys_canvas) return;
 
-    rotate_canvas(phys_canvas, logic_canvas_peri);
+    global_draw_count++;
+
+    // 1. Fill logic canvas White (Index 1)
+    lv_canvas_fill_bg(global_logic_canvas, lv_color_make(0, 0, 1), LV_OPA_COVER);
+
+    char buf[16];
+
+    // Top: Peripheral and Heartbeat
+    snprintf(buf, sizeof(buf), "PER%s", (global_draw_count % 2) ? "*" : " ");
+    draw_text(global_logic_canvas, 0, 0, buf);
+
+    // Connection Section
+    bool connected = zmk_split_bt_peripheral_is_connected();
+    draw_text(global_logic_canvas, 0, 20, "LNK");
+    draw_text(global_logic_canvas, 0, 30, connected ? "ON" : "OFF");
+
+    // Battery Section
+    draw_text(global_logic_canvas, 0, 50, "BAT");
+    snprintf(buf, sizeof(buf), "%d%%", get_natural_battery_level());
+    draw_text(global_logic_canvas, 0, 60, buf);
+
+    // Uptime Section
+    draw_text(global_logic_canvas, 0, 80, "UP:");
+    long long uptime_s = k_uptime_get() / 1000;
+    snprintf(buf, sizeof(buf), "%lld", uptime_s);
+    draw_text(global_logic_canvas, 0, 90, buf);
+
+    // 2. Rotate to physical
+    rotate_canvas(global_phys_canvas, global_logic_canvas);
 }
 
-static void battery_status_update_cb(struct battery_status_state state) {
-    struct zmk_widget_screen *widget;
-    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { draw_full_canvas(widget->obj); }
-}
-static struct battery_status_state battery_status_get_state(const zmk_event_t *eh) { 
-    return (struct battery_status_state){.level = zmk_battery_state_of_charge()};
-}
-ZMK_DISPLAY_WIDGET_LISTENER(widget_battery_status, struct battery_status_state, battery_status_update_cb, battery_status_get_state);
-ZMK_SUBSCRIPTION(widget_battery_status, zmk_battery_state_changed);
-ZMK_SUBSCRIPTION(widget_battery_status, zmk_usb_conn_state_changed);
+static void debug_work_handler(struct k_work *work) { draw_canvas(); }
+K_WORK_DEFINE(debug_work, debug_work_handler);
 
-static void peripheral_status_update_cb(struct peripheral_status_state state) {
-    struct zmk_widget_screen *widget;
-    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { draw_full_canvas(widget->obj); }
-}
-static struct peripheral_status_state get_state(const zmk_event_t *_eh) { return (struct peripheral_status_state){.connected = zmk_split_bt_peripheral_is_connected()}; }
-ZMK_DISPLAY_WIDGET_LISTENER(widget_peripheral_status, struct peripheral_status_state, peripheral_status_update_cb, get_state)
-ZMK_SUBSCRIPTION(widget_peripheral_status, zmk_split_peripheral_status_changed);
+static void debug_timer_handler(struct k_timer *timer) { k_work_submit(&debug_work); }
+K_TIMER_DEFINE(debug_timer, debug_timer_handler, NULL);
 
+LV_DRAW_BUF_DEFINE_STATIC(logic_draw_buf_peri, 128, 128, LV_COLOR_FORMAT_I1);
 LV_DRAW_BUF_DEFINE_STATIC(phys_draw_buf_peri, 128, 32, LV_COLOR_FORMAT_I1);
 
-int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent) {
+int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent)
+{
     widget->obj = lv_obj_create(parent);
     lv_obj_set_size(widget->obj, 128, 32);
-    lv_obj_set_style_bg_color(widget->obj, lv_color_white(), 0);
 
     lv_obj_t *phys_canvas = lv_canvas_create(widget->obj);
     lv_obj_align(phys_canvas, LV_ALIGN_TOP_LEFT, 0, 0);
     LV_DRAW_BUF_INIT_STATIC(phys_draw_buf_peri);
     lv_canvas_set_draw_buf(phys_canvas, &phys_draw_buf_peri);
-    
-    lv_color32_t c_white = {.red=255, .green=255, .blue=255, .alpha=255};
-    lv_color32_t c_black = {.red=0, .green=0, .blue=0, .alpha=255};
-    lv_canvas_set_palette(phys_canvas, 0, c_white);
-    lv_canvas_set_palette(phys_canvas, 1, c_black);
 
-    if (!logic_canvas_peri) {
-        LV_DRAW_BUF_INIT_STATIC(logic_draw_buf_peri);
-        logic_canvas_peri = lv_canvas_create(NULL);
-        lv_canvas_set_draw_buf(logic_canvas_peri, &logic_draw_buf_peri);
-        lv_canvas_set_palette(logic_canvas_peri, 0, c_white);
-        lv_canvas_set_palette(logic_canvas_peri, 1, c_black);
-    }
+    lv_canvas_set_palette(phys_canvas, 0, lv_color32_make(0, 0, 0, 255));
+    lv_canvas_set_palette(phys_canvas, 1, lv_color32_make(255, 255, 255, 255));
 
+    lv_obj_t *logic_canvas = lv_canvas_create(NULL);
+    LV_DRAW_BUF_INIT_STATIC(logic_draw_buf_peri);
+    lv_canvas_set_draw_buf(logic_canvas, &logic_draw_buf_peri);
+    lv_canvas_set_palette(logic_canvas, 0, lv_color32_make(0, 0, 0, 255));
+    lv_canvas_set_palette(logic_canvas, 1, lv_color32_make(255, 255, 255, 255));
+
+    global_phys_canvas = phys_canvas;
+    global_logic_canvas = logic_canvas;
+
+    widget->node.next = NULL;
     sys_slist_append(&widgets, &widget->node);
-    draw_full_canvas(widget->obj);
 
-    widget_battery_status_init();
-    widget_peripheral_status_init();
+    draw_canvas();
+    k_timer_start(&debug_timer, K_SECONDS(1), K_SECONDS(1));
 
     return 0;
 }
--- boards/shields/nice_oled/widgets/util.h
+++ boards/shields/nice_oled/widgets/util.h
@@ -7,21 +7,21 @@
 #define CANVAS_HEIGHT 128
 #define ROTATION_SIZE 128
 
-#define LVGL_BACKGROUND lv_color_make(0, 0, 1)
-#define LVGL_FOREGROUND lv_color_make(0, 0, 0)
+#define LVGL_BACKGROUND lv_color_make(0, 0, 0)
+#define LVGL_FOREGROUND lv_color_make(0, 0, 1)
 
 struct status_state {
-  uint8_t battery;
-  bool charging;
+    uint8_t battery;
+    bool charging;
 #if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
-  struct zmk_endpoint_instance selected_endpoint;
-  int active_profile_index;
-  bool active_profile_connected;
-  bool active_profile_bonded;
-  uint8_t layer_index;
-  const char *layer_label;
+    struct zmk_endpoint_instance selected_endpoint;
+    int active_profile_index;
+    bool active_profile_connected;
+    bool active_profile_bonded;
+    uint8_t layer_index;
+    const char *layer_label;
 #else
-  bool connected;
+    bool connected;
 #endif
 };
 
